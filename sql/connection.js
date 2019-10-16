1e69de88657d84982d24788a16d8f39b0bbfeb23
fagner henrique flores <henriquefagner@hotmail.com>
2019-10-15 19:03:06 -0500

    pushing deployment

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.70.189.224',
         user: 'root',
-        password: 'password',
+        password: 'lexilaura123',
         database: 'admin'
       }
 
